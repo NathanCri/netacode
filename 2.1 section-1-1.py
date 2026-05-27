ad7874ce5cb14b0c114e9970db6c4cdd9a1f6f16
NathanCri <nathancm@protonmail.com>
2026-05-27 19:37:50 -0300

    Add files via upload

---
+++ 2.1 section-1-1.py
@@ -0,0 +1,16 @@
+print("A strong man stand up for himself a stronger man stand up for others")
+print("Be the stronger man")
+print("-Ben")
+print()
+
+#with "\n"
+
+print("A stong man stand up for himself \na stonger man stand up for other \nBe the stonger man \n-Ben")
+print()
+
+print("hii im", "Terry Davis", end=" ")
+print("how about you?")
+
+
+print("i", "love", "you", "so", "i", "can", "never", "let", "you", "go", sep="-")
+
